4312441926a9578d4c47d8590af6e5ac5093b4de
sinharakesh <rksinha.btech@gmail.com>
2017-01-22 11:41:46 +0530

    test page and todo updated

--- admin/page/test.php
+++ admin/page/test.php
@@ -29,20 +29,24 @@ class page_test extends Page {
         	
         // }
 
-        $grid = $this->add('Grid');
-        $model = $this->add('Model_Restaurant');
-        $current_lat = 24.586885;
-        $current_long = 73.69917499999997;
-        // $q = $model->dsql();
-        // $latlng = $q->expr('ABS(ABS([0] - [1]) + ABS([2] - [3]))',[$model->getElement('latitude'),$current_lat,$model->getElement('longitude'),$current_long]);
-        $model->addExpression('distance')->set(function($m,$q)use($current_lat,$current_long){
-            return $q->expr('ABS(ABS([0] - [1]) + ABS([2] - [3]))',[$m->getElement('latitude'),$current_lat,$m->getElement('longitude'),$current_long]);
-        });
-        $model->addCondition('distance',"<=",10);
-        $model->setOrder('distance','asc');
+        // $grid = $this->add('Grid');
+        // $model = $this->add('Model_Restaurant');
+        // $current_lat = 24.586885;
+        // $current_long = 73.69917499999997;
+        // // $q = $model->dsql();
+        // // $latlng = $q->expr('ABS(ABS([0] - [1]) + ABS([2] - [3]))',[$model->getElement('latitude'),$current_lat,$model->getElement('longitude'),$current_long]);
+        // $model->addExpression('distance')->set(function($m,$q)use($current_lat,$current_long){
+        //     return $q->expr('ABS(ABS([0] - [1]) + ABS([2] - [3]))',[$m->getElement('latitude'),$current_lat,$m->getElement('longitude'),$current_long]);
+        // });
+        // $model->addCondition('distance',"<=",10);
+        // $model->setOrder('distance','asc');
 
-        $grid->setModel($model,['name','distance']);
-        $grid->addPaginator($ipp=15);
+        // $grid->setModel($model,['name','distance']);
+        // $grid->addPaginator($ipp=15);
+
+        $table = $this->add('Model_ReservedTable')->load(214);
+        $table->sendEnquiryEmailToHost();
+
 
     }
 
