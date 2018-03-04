9d69088bfeb9620d0cb12ac6a89ad92e4fff1c2d
sinharakesh <rksinha.btech@gmail.com>
2018-03-04 14:01:23 +0530

    before save restaurant data updated

--- shared/lib/Model/Favorities.php
+++ shared/lib/Model/Favorities.php
@@ -12,10 +12,20 @@
 		$this->addField('longitude');
 		$this->addField('latitude');
 		$this->addField('slug_url');
-		
+
 		$this->addField('created_at')->type('datetime')->defaultValue(date('Y-m-d H:i:s'));
 
 		$this->add('dynamic_model/Controller_AutoCreator');
+		$this->addHook('beforeSave',$this);
+	}
+
+	function beforeSave(){
+		if($this['restaurant_id']){
+			$rest = $this->add('Model_Restaurant')->load($this['restaurant_id']);
+			$this['longitude'] = $rest['longitude'];
+			$this['latitude'] = $rest['latitude'];
+			$this['slug_url'] = $rest['url_slug'];
+		}
 	}
 }
  
